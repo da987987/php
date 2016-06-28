6f23c90819c52e4315d682767a785111e9d7dcfb
格野 <da987987@gmail.com>
2016-06-28 13:44:56 +0800

    2. class 增加內容物

--- class.php
+++ class.php
@@ -15,7 +15,52 @@ and open the template in the editor.
         <?php
         class cat
         {
+            public $name;//屬性
+            protected $position;
+            public function __construct($name)//方法，建構子
+            {
+                $this->name=$name;
+                $this->position = ['x'=>0,'y'=>0];
+            }
+            public function moveTo($x,$y)
+            {
+                $this->position['x'] = $x;
+                $this->position['y'] = $y;
+                return $this->position;
+            }
+            public function resetPosition()
+            {
+                $this->moveTo(0,0);
+                return $this->position;
+            }
         }
+        $pet = new cat("Kitty");
+        echo $pet->name;
+        echo "<br>";
+        $pet->name="Hallo kitty";
+        echo $pet->name;
+        echo "<br>";
+        echo "(".$movePosition['x'].",".$movePosition['y'].")";
+        $movePosition = $pet->moveTo(3, 5);
+        echo "(".$movePosition['x'].",".$movePosition['y'].")";
+        echo "<br>";
+        $movePosition = $pet->resetPosition();
+        echo "(".$movePosition['x'].",".$movePosition['y'].")";
+        
+       
+        
+        
+        class dog
+        {
+            public function moveTo($x,$y)
+            {
+                $this->position['x']=$x;
+                $this->position['y']=$y;
+                return $this->position;
+            }           
+        }
+        
+        
         ?>
     </body>
 </html>
